f3bfef0281c215df08e059c4de343cb19a24c8a1
hakaneroztekin <eroztekin@itu.edu.tr>
2018-10-02 14:45:08 +0300

    added basic teacher entity

--- src/main/java/com/codethecode/courseregistersystem/entity/Teacher.java
+++ src/main/java/com/codethecode/courseregistersystem/entity/Teacher.java
@@ -1,7 +1,40 @@
 package com.codethecode.courseregistersystem.entity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-public class Teacher {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teacher{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    private Long id;
+
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="surname")
+    private String surname;
+
+    @Column(name="gender")
+    private String gender;
+
+    @Column(name="branch")
+    private String branch;
+
+    @Column(name="hourly_cost")
+    private Long cost;
+
+    @Column(name="balance")
+    private Long balance;
+
 }
+
+
