a319ae10e26d5efba25c11d6de63853dd9df709a
Cemal <cigdemc@itu.edu.tr>
2018-10-05 16:10:40 +0300

    course entity is added

--- src/main/java/com/codethecode/courseregistersystem/entity/Course.java
+++ src/main/java/com/codethecode/courseregistersystem/entity/Course.java
@@ -1,7 +1,50 @@
 package com.codethecode.courseregistersystem.entity;
 
-import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "branch")
+    private String branch;
+
+    @Column(name = "date")
+    private String date;
+
+    @Column(name = "beginningTime")
+    private String beginningTime;
+
+    @Column(name = "period") //dersin kaç saat süreceği
+    private String period;
+
+    @Column(name = "teacherName") //Dersi verecek hocanın adı
+    private String teacherName;
+
+    @Column(name = "teacherSurname")
+    private String teacherSurname;
+
+    @Column(name = "studentName") //Dersi alacak öğrencinin adı
+    private String studentName;
+
+    @Column(name = "studentSurname")
+    private String studentSurname;
+
+    @Column(name = "teacherID")// ID ler öğrenci ve öğretmenle dersin bağlantısını göstermek için.
+    private String teacherID;  // Ad soyad da var ama, aynı isimde öğrenci ve öğretmenler olabilir diye ID de ekledim.
+
+    @Column(name = "studentID")
+    private String studentID;
+
 }
